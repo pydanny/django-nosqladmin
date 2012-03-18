b169652e64919c843dc2f7b4840c561abefdccb1
Daniel Greenfeld <pydanny@gmail.com>
2012-03-18 16:29:49 -0700

    making the wiring behave better

--- examples/blog/mongoadmin.py
+++ examples/blog/mongoadmin.py
@@ -2,3 +2,6 @@ from nosqladmin.mongodb import MongoAdmin
 
 class PostAdmin(MongoAdmin):
     collection_name = "example_blog.post"
+    
+    def has_add_permission(self, request):
+        return True
--- nosqladmin/views.py
+++ nosqladmin/views.py
@@ -5,7 +5,14 @@ import pymongo
 from nosqladmin.mixins import NosqlAdminViewMixin
 
 # TODO Move this to settings or something...
-CONNECTION = pymongo.Connection("localhost", 27017)
+# TODO Move this to settings or something...
+# TODO Move this to settings or something...
+from pymongo import Connection
+from pymongo.errors import CollectionInvalid
+CONNECTION = Connection()
+# End setup
+# End setup
+# End setup
 
 class IndexView(NosqlAdminViewMixin, ListView):
 
@@ -28,4 +35,5 @@ class CollectionListView(NosqlAdminViewMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super(CollectionListView, self).get_context_data(**kwargs)            
         context['collection_name'] = self.collection_name
+        context = self.set_permissions_in_context(context)
         return context
