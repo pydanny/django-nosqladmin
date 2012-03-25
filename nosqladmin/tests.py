f0b3dcb87e61c98505664e699ad3c1a254fcba67
Daniel Greenfeld <pydanny@gmail.com>
2012-03-25 09:50:49 -0700

    TODO on tests

--- nosqladmin/tests.py
+++ nosqladmin/tests.py
@@ -1,16 +1,3 @@
 """
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
+TODO put real tests here
 """
-
-from django.test import TestCase
-
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
